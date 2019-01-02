1ae0b660e157ffd2b225f0df610803b9a6b6d5a8
Krzysztof Bujak <34073209+Andy1Blue@users.noreply.github.com>
2019-01-02 19:42:27 +0100

    adding the initial files, template (ejs) and css style file

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Pizza delivery system'
+  });
+});
+
+// Export the module
+module.exports = router;
